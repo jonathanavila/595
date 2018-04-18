e690b318b56d887e704ef67d30c346825fa9716e
jonathanavila <jonathan.r.avila@gamil.com>
2018-04-18 17:43:35 -0400

    AJAX Request for putting temperature on arduino

--- requests.js
+++ requests.js
@@ -1,15 +1,16 @@
 	
-var lastStatType = "Actual"
+var lastStatType = "C"
 var lastTimeFrame = "10"
 var onOff = "On"
 var lastTemperatureType = "Celsius"
+var lastTemp;
 
 $(document).ready(function() {
 
 	$('button').on('click', function() {
 		var buttonPressed = $(this);
 		if (buttonPressed.attr("class") == "Temperature") {
-			console.log("Sending GET localhost:3001/ color")
+			console.log("Sending GET localhost:3001/color")
 			if  (lastTemperatureType == "Celsius") {
 				lastTemperatureType = "Fahrenheit"
 			} else {
@@ -24,7 +25,24 @@ $(document).ready(function() {
 			getStat();
 
 		} else if (buttonPressed.attr("class") == "onoff") {
-			console.log(temps[0])
+			if (onOff == "On") {
+				onOff = "Off"
+				clearInterval(interChart);
+				document.getElementById("display").value = "";
+				$.ajax({
+					type: "GET",
+					url: "localhost:3001/off"
+				});
+			} else {
+				onOff = "On"
+				getStat();
+				interChart = setInterval(function() {
+					updateChart();}, 1000);
+				}
+				$.ajax({
+					type: "GET",
+					url: "localhost:3001/on"
+				});
 		}
 		updateData();
 	})
@@ -50,13 +68,13 @@ $(document).ready(function() {
 });
 
 getStat = function() {
-	if (lastStatType == "High") {
+	if (lastStatType == "H") {
 		getHigh();
 	}
-	else if (lastStatType == "Low") {
+	else if (lastStatType == "L") {
 		getLow();
 	}
-	else if (lastStatType == "Average") {
+	else if (lastStatType == "A") {
 		getAverage();
 	}
 	else {
@@ -80,8 +98,6 @@ getAverage = function() {
 	}
 	avg = sum / num
 	document.getElementById("display").value = convertTemperature(avg);
-
-
 	
 }
 getActual = function() {
@@ -129,8 +145,23 @@ getLow = function() {
 }
 
 convertTemperature = function(val) {
-	if (lastTemperatureType == "Celsius") return val;
-	else return 32 + (9*val)/5;
+	if (lastTemperatureType == "Celsius") {
+		lastTemp = val;
+		return val;
+	}
+	else {
+		lastTemp = 32 + (9*val)/5;
+		return lastTemp;
+	}
+}
+
+convertTempAppended = function() {
+	if (lastTemperatureType == "Celsius") {
+		return lastTemp.toFixed(1)+"C";
+	}
+	else {
+		return lastTemp.toFixed(1)+"F";
+	} 
 }
 
 
